be5477a5f2a8d822cd23696b50a87b93d81e8a7d
JorgeHNovoa <153151180+JorgeHNovoa@users.noreply.github.com>
2023-12-15 23:22:38 -0300

    a
    wea

--- Sim annealing model/testing_sim_nuevo_costo.py
+++ Sim annealing model/testing_sim_nuevo_costo.py
@@ -13,6 +13,8 @@ def normalize_array(arr):
     normalized_array = -1 + 2 * (arr - min_val) / (max_val - min_val)
     return normalized_array
 
+def generate_random_matrix(size):
+    return np.random.uniform(low=-1, high=1, size=(size,size))
 
 
 # Función para generar un terreno inicial aleatorio usando ruido Perlin
@@ -31,54 +33,53 @@ def generate_perlin_terrain(size, scale, octaves, persistence, lacunarity, seed)
             terrain[i][j] = noise.pnoise2(i/scale, j/scale, octaves=octaves, persistence=persistence, lacunarity=lacunarity, repeatx=size, repeaty=size, base=seed)
     return terrain
 
-def generate_random_matrix(size):
-    return np.random.uniform(low=-1, high=1, size=(size,size))
+def costo_terreno(terreno):
+    """
+    Función de costo para suavizar un terreno en Simulated Annealing.
 
-# Función de evaluación basada en la pendiente del terreno
-def evaluate_terrain(terrain):
-    # Calcular la pendiente del terreno
+    Parámetros:
+    - terreno: numpy array, matriz que representa el terreno con alturas.
 
-    gradient_x, gradient_y = np.gradient(terrain)
-    val = gradient_x**2 + gradient_y**2
-     
-    slope = np.sqrt(gradient_x**2 + gradient_y**2)
-    # En este ejemplo, la función de evaluación es la suma de las pendientes
-    return np.sum(slope)
+    Retorna:
+    - float, valor del costo.
+    """
+    # Ajusta los pesos según sea necesario
+    w_homogeneidad = 1.0
+    w_pendiente = 1.0
+    w_extremos = 1.0
+    
+    # Parámetro para evitar divisiones por cero
+    epsilon = 1e-8
 
-def terrain_cost_proximity(terrain, threshold=0.3, penalty_factor=10, proximity_factor=10):
-    # Compute the gradient of the terrain
-    gradient_x, gradient_y = np.gradient(terrain)
+    # Calcula la homogeneidad local (puedes ajustar la ventana según tus necesidades)
+    homogeneidad_local = np.std(terreno, axis=(0, 1))
 
-    # Calculate the magnitude of the gradient vector at each point
-    slope = np.sqrt(gradient_x**2 + gradient_y**2)
+    # Calcula la penalización de homogeneidad
+    penalizacion_homogeneidad = 1.0 / (np.mean(homogeneidad_local) + epsilon)
 
-    # Apply a penalty for slopes above the threshold
-    threshold_reward = np.maximum(0, slope - threshold)
+    # Calcula la penalización de pendiente en ambas direcciones (horizontal y vertical)
+    penalizacion_pendiente = np.sum(np.abs(np.diff(terreno, axis=0))) + np.sum(np.abs(np.diff(terreno, axis=1)))
 
-    # Calculate penalties based on proximity of values
-    proximity_penalty = proximity_factor * np.sum(np.abs(np.diff(terrain, axis=0)))
-    proximity_penalty += proximity_factor * np.sum(np.abs(np.diff(terrain, axis=1)))
-
-    # Calculate the overall cost as the sum of penalties
-    cost = np.sum(threshold_reward) * penalty_factor - proximity_penalty
-
-    return cost
+    # Calcula la recompensa de extremos
+    extremos = np.array([terreno[0, 0], terreno[0, -1], terreno[-1, 0], terreno[-1, -1]])
+    recompensa_extremos = np.sum(np.exp(-np.abs(np.tile(extremos, len(terreno.flatten()) // 4) - terreno.flatten())**2))
 
 
 
+    # Calcula el costo total
+    costo_total = w_homogeneidad * penalizacion_homogeneidad + w_pendiente * penalizacion_pendiente + w_extremos * recompensa_extremos
+
+    return costo_total
 
 # Simulated Annealing para generar terreno realista
 def simulated_annealing(initial_terrain, iterations, initial_temperature, cooling_rate):
     current_terrain = initial_terrain.copy()
-    current_energy = terrain_cost_proximity(current_terrain)
-    a,b = np.shape(initial_terrain)
-
-
+    current_energy = costo_terreno(current_terrain)
 
     for iteration in range(iterations):
         # Generar un nuevo terreno vecino
-        new_terrain = current_terrain + np.random.normal(loc = 0.3, scale=0.01, size=current_terrain.shape)
-        new_energy = terrain_cost_proximity(new_terrain)
+        new_terrain = current_terrain + generate_random_matrix(size=current_terrain.shape)
+        new_energy = costo_terreno(new_terrain)
 
         # Calcular la diferencia de energía
         energy_difference = new_energy - current_energy
@@ -94,16 +95,15 @@ def simulated_annealing(initial_terrain, iterations, initial_temperature, coolin
     return current_terrain
 
 # Parámetros
-terrain_size = 200
-
+terrain_size = 500
 scale = 20.0
 octaves = 6
 persistence = 0.5
 lacunarity = 2.0
 seed = 42
-iterations = 5000
+iterations = 1000
 initial_temperature = 1.0
-cooling_rate = 0.001
+cooling_rate = 0.7
 
 # Generar terreno inicial usando ruido Perlin
 initial_terrain = generate_random_matrix(terrain_size)
@@ -111,7 +111,6 @@ initial_terrain = generate_random_matrix(terrain_size)
 # Aplicar Simulated Annealing
 final_terrain = simulated_annealing(initial_terrain, iterations, initial_temperature, cooling_rate)
 final_terrain = normalize_array(final_terrain)
-
 plt.figure(figsize=(12, 6))
 
 plt.imshow(initial_terrain, cmap='terrain', origin='lower')
